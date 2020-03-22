c4d4d881a75ea5af2157e0a610460e5d6d66b0c9
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-03-22 13:57:09 +0100

    AssemblyAttrDemo

---
+++ AssemblyAttrDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            Type type = Type.GetType("System.Reflection.AssemblyDescriptionAttribute");
+            object[] attributes = currentAssembly.GetCustomAttributes(type, false);
+
+            if (attributes.Length > 0)
+            {
+                AssemblyDescriptionAttribute attribute = (AssemblyDescriptionAttribute)attributes[0];
+                Console.WriteLine(attribute.Description);
+                Console.ReadLine();
+            }
+        }
+    }
+}
