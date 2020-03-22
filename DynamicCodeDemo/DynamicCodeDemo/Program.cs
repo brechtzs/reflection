91943e3177dcb6f773e29d2fe0618957351debdd
Zsuzsa Engelbrecht <zsuzsa.engelbrecht@gmail.com>
2020-03-22 16:11:29 +0100

    DynamicCodeDemo

---
+++ DynamicCodeDemo/DynamicCodeDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Web.dll";
+            Assembly assembly = Assembly.LoadFile(path);
+
+            Type type = assembly.GetType("System.Web.HttpUtility");
+            
+            //????????? 2nd paramether - new Type object
+            MethodInfo encode = type.GetMethod("HtmlEncode", new Type[] { typeof(string) });
+            MethodInfo decode = type.GetMethod("HtmlDecode", new Type[] { typeof(string) });
+
+            string stringToBeEncoded = "fsdhdfgdjhsfgdjhsfgk 77 && <>";
+            Console.WriteLine(stringToBeEncoded);
+
+            string encodedString = (string)encode.Invoke(null, new object[] { stringToBeEncoded });
+            Console.WriteLine(encodedString);
+
+            string decodedString = (string)decode.Invoke(null, new object[] { encodedString });
+            Console.WriteLine(decodedString);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
